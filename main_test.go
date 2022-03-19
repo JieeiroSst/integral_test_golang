4dab3e71ff1eddd50c64cb9829d863926929a54e
Alain Sst <AlainLuu.1999@gmail.com>
2022-03-19 16:00:14 +0700

    .

---
+++ main_test.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"testing"
+	"errors"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/assert"
+)
+
+type MockDatabase struct {
+	mock.Mock
+}
+
+func (db *MockDatabase) connect() error {
+	args := db.Called()
+	return args.Error(0)
+}
+
+func (db *MockDatabase) sendMessage(message *string) error {
+	args := db.Called(message)
+	return args.Error(0)
+}
+
+func TestSuccess(t *testing.T) {
+	db := new(MockDatabase)
+	message := "Hello"
+
+	// Set expectations
+	db.On("connect").Return(nil)
+	db.On("sendMessage", &message).Return(nil)
+
+	err := Talk(db, &message)
+
+	assert.Equal(t, nil, err, "No error")
+	db.AssertExpectations(t)
+}
+
+func TestErrorOnConnect(t *testing.T) {
+	db := new(MockDatabase)
+
+	// Set expectations
+	db.On("connect").Return(errors.New("Some error"))
+
+	message := "Hello"
+	err := Talk(db, &message)
+
+	assert.NotEqual(t, nil, err, "An error is thrown if connection fails")
+	db.AssertExpectations(t)
+}
+
+func TestErrorOnMessage(t *testing.T) {
+	db := new(MockDatabase)
+	message := "Hello"
+
+	// Set expectations
+	db.On("connect").Return(nil)
+	db.On("sendMessage", &message).Return(errors.New("Some error"))
+
+	err := Talk(db, &message)
+
+	assert.NotEqual(t, nil, err, "An error is thrown if sendMessage fails")
+	db.AssertExpectations(t)
+}
